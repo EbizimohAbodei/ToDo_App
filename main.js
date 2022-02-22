4bb27865c4aa39dd3ce18c9ba9b5fa5a4963424e
EbizimohAbodei <vincentkendy@gmail.com>
2022-02-22 11:35:36 +0200

    Fixed empty input bug

--- main.js
+++ main.js
@@ -3,41 +3,49 @@ const form = document.querySelector("form"); //add button
 const list = document.querySelector(".todo__list");
 const btns = document.querySelector(".item__button");
 let listItem = document.querySelector(".listItem");
-let completeButton = document.querySelector(".completed__button");
 let deleteButton = document.querySelector("#trash__button");
+let allBtns = document.querySelectorAll(".item__button");
 
 //New Task List item
 
 let tasks = [];
+let addedItems;
 
 form.addEventListener("submit", (event) => {
-  tasks.unshift(taskInput.value);
   console.log(tasks);
   event.preventDefault();
 
   // create List Item
-  if (tasks.length > 0 && taskInput.value !== "") {
-    list.innerHTML = "";
-    tasks.forEach(function (item) {
-      list.insertAdjacentHTML(
-        "beforeend",
-        `<div><li class=list__Item>${item}</li><button class="item__button" id="completed__button"><i class="fas fa-check"></i>
+  if (taskInput.value !== "") {
+    tasks.unshift(taskInput.value);
+  } else {
+    window.alert(
+      "Input field cannot be empty \nEnter the description of the task"
+    );
+    tasks = [...tasks];
+  }
+
+  list.innerHTML = "";
+  tasks.forEach(function (item) {
+    list.insertAdjacentHTML(
+      "beforeend",
+      `<div><li class=list__Item>${item}</li><button class="item__button" id="completed__button"><i class="fas fa-check"></i>
         </button><button class="item__button" id="edit__button">
         <i class="uil uil-pen"></i>
         </button><button class="item__button" id="trash__button">
         <i class="fas fa-trash"></i>
         </button>
       <div>`
-      );
-      taskInput.value = "";
-    });
-  } else {
-    window.alert("Input field cannot be empty");
-    tasks = [];
-  }
+    );
+    addedItems = list;
+    console.log(addedItems);
+    taskInput.value = "";
+  });
 });
 
-// completeButton.addEventListener("click", () => {});
+// completeButton.addEventListener("click", () => {
+//   console.log("working");
+// });
 
 // deleteButton.addEventListener("click", (e) => {
 //   console.log(e);
